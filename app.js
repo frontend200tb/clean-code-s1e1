a3c3fe6114a2ec23810b06d18e3b2f27c5887f3a
frontend200tb <frontend200tb@mail.ru>
2024-01-07 00:33:12 +0300

    refactor(extended-2.1): add bem

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
-  listItem.classList.add('list-item');
+  listItem.classList.add('list__item');
   label.innerText=taskString;
   label.classList.add('task-name');
 
